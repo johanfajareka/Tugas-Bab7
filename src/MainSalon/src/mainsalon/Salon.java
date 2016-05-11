102bfc6acf9630a757a8b6928fc085fde5e9f32a
Johan Fajar <johanrubik@gmail.com>
2016-05-11 18:58:12 +0700

    2

--- src/MainSalon/src/mainsalon/Salon.java
+++ src/MainSalon/src/mainsalon/Salon.java
@@ -17,5 +17,23 @@ public abstract class Salon {
     final double sabun = 40000;
     final double kondisioner = 25000;
 
-    
+    public Salon(String nama, String id, String alamat, String telp) {
+        this.nama = nama;
+        this.id = id;
+        this.alamat = alamat;
+        this.telp = telp;
+    }
+
+    public void display() {
+        System.out.println("===== >> Total Belanja << =====");
+        System.out.println("");
+        System.out.println("Nama        : " + nama);
+        System.out.println("No. ID      : " + id);
+        System.out.println("Alamat      : " + alamat);
+        System.out.println("No. Telp    : " + telp);
+        System.out.println("Diskon      : " + diskon);
+    }
+
+    public abstract double harga();
+
 }

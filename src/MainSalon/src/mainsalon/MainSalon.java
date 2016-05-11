13b494ece167aa39dc24ff0991ed2e3ccd59a16d
Johan Fajar <johanrubik@gmail.com>
2016-05-11 19:24:48 +0700

    case3

--- src/MainSalon/src/mainsalon/MainSalon.java
+++ src/MainSalon/src/mainsalon/MainSalon.java
@@ -111,7 +111,48 @@ public class MainSalon {
                             }
 
                         }
-                    
+                    case 3:
+                        for (;;) {
+                            Silver a = new Silver(nama, id, alamat, telp);
+                            a.diskon = "10%";
+                            System.out.println("");
+                            System.out.println("================= >>> MENU <<< =================");
+                            System.out.println("1. Gunakan Layanan "
+                                    + "(Krimbat = Rp.50.000,- + "
+                                    + " Smothing = Rp.80.000,- + "
+                                    + " Rebonding = Rp.70.000,-)");
+                            System.out.println("2. Beli Produk "
+                                    + "(Sampo = Rp.30.000,- + "
+                                    + " Sabun = Rp.40.000,- + "
+                                    + " Kondisioner = Rp.25.000,-)");
+                            System.out.println("3. Lihat Harga");
+                            System.out.println("4. Keluar Program");
+                            System.out.print("Masukkan Pilihan : ");
+                            pil = in.nextInt();
+                            switch (pil) {
+                                case 1:
+                                    a.layanan();
+                                    break;
+                                case 2:
+                                    a.produk();
+                                    break;
+                                case 3:
+                                    a.display();
+                                    System.out.println("Harga Layanan = Rp " + a.layanan());
+                                    System.out.println("Harga Produk  = Rp " + a.produk());
+                                    System.out.println("Total Harga   = Rp " + a.harga());
+                                    break;
+                                case 4:
+                                    System.exit(0);
+                                default:
+                                    System.out.println("Masukkan pilihan yang benar...");
+                            }
+
+                        }
+                    default:
+                        System.out.println("Masukkan Pilihan Yang Benar");
+                }
+            }
                     
     }
 

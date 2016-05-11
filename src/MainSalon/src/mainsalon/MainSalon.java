7ba0ebdc83c4594f37b72094517ece088b369377
Johan Fajar <johanrubik@gmail.com>
2016-05-11 19:04:48 +0700

    7 (kebanyaakan sebelumnya langsung saya copy mas, soalnya udah selesai juga
    bikin repositorinya telat soalnya mas,,,

--- src/MainSalon/src/mainsalon/MainSalon.java
+++ src/MainSalon/src/mainsalon/MainSalon.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainsalon;
 
-/**
- *
- * @author ASUS-A455L
- */
+import java.util.Scanner;
+
 public class MainSalon {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        String jawab;
+        int pil;
+        String nama;
+        String id;
+        String alamat;
+        String telp;
     }
-    
+
 }

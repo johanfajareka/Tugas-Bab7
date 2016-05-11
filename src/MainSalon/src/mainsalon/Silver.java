19529c44fce34aba90f12c83487b1fcac8b3ff14
Johan Fajar <johanrubik@gmail.com>
2016-05-11 19:00:30 +0700

    5
    hy hy

--- src/MainSalon/src/mainsalon/Silver.java
+++ src/MainSalon/src/mainsalon/Silver.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainsalon;
 
-/**
- *
- * @author ASUS-A455L
- */
-public class Silver {
-    
+public class Silver extends Salon {
+
+    public Silver(String nama, String id, String alamat, String telp) {
+        super(nama, id, alamat, telp);
+    }
+
+    public double layanan() {
+        layanan = (krimbat + smothing + rebounding) * 0.9;
+        return layanan;
+    }
+
+    public double produk() {
+        produk = (sampo + sabun + kondisioner) * 0.9;
+        return produk;
+    }
+
+    @Override
+    public double harga() {
+        total = layanan() + produk();
+        return total;
+    }
+
 }

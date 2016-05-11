21ac41b8ed0a1eadc2b5385f3eaf91a985afa551
Johan Fajar <johanrubik@gmail.com>
2016-05-11 18:57:09 +0700

    1

--- src/MainSalon/src/mainsalon/Salon.java
+++ src/MainSalon/src/mainsalon/Salon.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainsalon;
 
-/**
- *
- * @author ASUS-A455L
- */
-public class Salon {
+public abstract class Salon {
+
+    private String nama;
+    private String id;
+    private String alamat;
+    private String telp;
+    String diskon;
+    double total;
+    double layanan;
+    final double krimbat = 50000;
+    final double smothing = 80000;
+    final double rebounding = 70000;
+    double produk;
+    final double sampo = 30000;
+    final double sabun = 40000;
+    final double kondisioner = 25000;
+
     
 }
